92dd236a22bc18566029a6bd25e04cc56b058dbe
Josh Tillinghast <jwtill@icloud.com>
2022-03-15 10:01:05 -0700

    Create Models and other boilerplate files
    Co-authored-by: Will Greenberg <greenberg.will@gmail.com>

---
+++ Registrar/Models/Student.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+namespace Registrar.Models
+{
+  public class Student
+  {
+    public Student()
+    {
+      this.JoinEntities = new HashSet<CourseStudent>();
+    }
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public string Date { get; set; }
+    public virtual ICollection<CourseStudent> JoinEntities { get; }
+  }
+}
